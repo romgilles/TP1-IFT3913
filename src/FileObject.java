db7d98fc6f277917f103c0c4483430b2bb32c799
Mahmoud <79349416+Mahmoud-L@users.noreply.github.com>
2023-09-27 16:28:23 -0400

    Update FileObject.java
    - Ensures that the program can correctly parse and sort these numbers during processing.

--- src/FileObject.java
+++ src/FileObject.java
@@ -1,3 +1,6 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
 public class FileObject {
     private String path;
     private String packageName;
@@ -12,12 +15,22 @@ public class FileObject {
         this.name = name;
         this.tloc = tloc;
         this.tassert = tassert;
-        this.tcmp = (double) tloc / tassert;
+         // Vérification pour éviter la division par zéro 
+        if (tassert == 0) {
+            this.tcmp = 0;
+        } else {
+            this.tcmp = (double) tloc / tassert;
+        }
     }
 
     public String toCsv() {
-        return String.format("%s,%s,%s,%d,%d,%.2f", this.path, this.packageName, this.name, this.tloc, this.tassert,
-                this.tcmp);
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setDecimalSeparator('.');
+        DecimalFormat format = new DecimalFormat("#.##", symbols);
+        
+        return String.format("%s,%s,%s,%d,%d,%s", this.path, this.packageName, this.name, this.tloc, this.tassert,
+                format.format(this.tcmp));
+    
     }
 
 }
